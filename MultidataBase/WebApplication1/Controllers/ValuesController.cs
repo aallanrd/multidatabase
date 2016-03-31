1b8e521f08e7c766c3133b59009f205bca11ee15
Allan Rojas <allanjose91@gmail.com>
2016-03-31 11:52:59 -0600

    Section

--- MultidataBase/WebApplication1/Controllers/ValuesController.cs
+++ MultidataBase/WebApplication1/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
-namespace WebApp.Controllers
+namespace WebApplication1.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
--- MultidataBase/WebApplication1/Startup.cs
+++ MultidataBase/WebApplication1/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace WebApp
+namespace WebApplication1
 {
     public class Startup
     {
