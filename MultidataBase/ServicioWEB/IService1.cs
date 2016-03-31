7fb600cf90824c8ade7ffc41f4499ca9650d78d9
Allan Rojas <allanjose91@gmail.com>
2016-03-31 12:21:11 -0600

    Test

---
+++ MultidataBase/ServicioWEB/IService1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServicioWEB
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetMariaData(int value, string table, string db);
+
+        [OperationContract]
+        aMariaTipo GetMariaDataUsingDataContract(aMariaTipo tipo);
+
+        // TODO: Add your service operations here
+
+        [OperationContract]
+        string GetSQLData(int value, string table, string db);
+
+        [OperationContract]
+        aMariaTipo GetSQLDataUsingDataContract(aMariaTipo tipo);
+
+        [OperationContract]
+        string GetMongoData(int value, string table, string db);
+
+        [OperationContract]
+        aMariaTipo GetMongoDataUsingDataContract(aMariaTipo tipo);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "Servicio.ContractType".
+    [DataContract]
+    public class aMariaTipo
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ MultidataBase/ServicioWEB/Service1.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServicioWEB
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetMariaData(int value, string table, string db)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public aMariaTipo GetMariaDataUsingDataContract(aMariaTipo composite)
+        {
+            return composite;
+        }
+        public string GetSQLData(int value, string table, string db)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public aMariaTipo GetSQLDataUsingDataContract(aMariaTipo composite)
+        {
+            return composite;
+        }
+        public string GetMongoData(int value, string table, string db)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public aMariaTipo GetMongoDataUsingDataContract(aMariaTipo composite)
+        {
+            
+            return composite;
+        }
+    }
+}
