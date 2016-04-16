2058ed559f21947f1856f7f4020122e9fc2c4c7c
Allan Rojas <allanjose91@gmail.com>
2016-04-16 12:17:03 -0600

    Change

--- MultidataBase/WebApp/App_Start/BundleConfig.cs
+++ MultidataBase/WebApp/App_Start/BundleConfig.cs
@@ -21,7 +21,11 @@ namespace WebApp
                       "~/Scripts/respond.js",
                       "~/Scripts/jquery.tooltipster.min.js"));
 
-           
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                   "~/bower_components/angular/angular.min.js",
+                   "~/Scripts/app.js"));
+
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ MultidataBase/WebApp/Scripts/app.js
@@ -0,0 +1,19 @@
+﻿var multiDBApp = angular.module('multiDBApp', []);
+
+multiDBApp.controller('multiController', function ($scope) {
+
+    $scope.list = [
+      {
+          'name': 'Nexus S',
+          'port': 1
+      },
+      {
+          'name': 'Motorola XOOM™ with Wi-Fi',
+          'port': 2
+      },
+      {
+          'name': 'MOTOROLA XOOM™',
+          'port': 3
+      }
+    ];
+});
